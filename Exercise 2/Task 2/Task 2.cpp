f6f0d3ac925d85d2a63719256bd335a9e55d8844
Josip Condic Jurkic <51995137+ararune@users.noreply.github.com>
2021-11-28 17:56:00 +0100

    Update Task 2.cpp

--- Exercise 2/Task 2/Task 2.cpp
+++ Exercise 2/Task 2/Task 2.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
 using namespace std;
-// function that returns reference to an array element, using return as lvalue increment the n-th element by 1
-int &retref(int i, int *arr)
+
+int& retref(int* arr, int i)
 {
     return arr[i];
 }
 
 int main()
 {
-    int n, i;
     cout << "Enter the size of the array : ";
-    cin >> n; 
-    int* arr = new int[n]; 
-
-    cout << "Enter the elements : ";
+    int n;  cin >> n;
+    cout << "Enter " << n << " elements into the array : ";
+    int* arr = new int[n];
     for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];;
-    }
-    cout << "Enter i : ";
-    cin >> i;
+        cin >> arr[i];
+    cout << "Enter index of the element : ";  int i;  cin >> i;
 
-	int x = 5;
+    cout << "\nValue after the index = " << ++retref(arr, i);
 
-	cout << "++x: " << ++x;
-	cout << "x++: " << x++;
-
-    cout << "\ni + 1 = " << ++retref(i, arr);
-
+    delete[] arr;
 }
+
+
