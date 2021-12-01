186880812b777c7eebdb3ab97ac092512342040f
Josip Condic Jurkic <51995137+ararune@users.noreply.github.com>
2021-12-01 15:38:02 +0100

    Update Task 6.cpp

--- Exercise 3/Task 6/Task 6.cpp
+++ Exercise 3/Task 6/Task 6.cpp
@@ -3,17 +3,15 @@
 #include <vector>
 #include <algorithm>
 // Push user input strings to a vector of string, reverse each string then sort vector in lexicographic order
-using namespace std;
 
 /* take string input (ctrl+z to end)
    reverse the strings
-   sort them
-   return vector of strings*/
-void reverseAndSort(vector<string>& myString)
+   sort them */
+void reverseAndSort(std::vector<std::string>& myString)
 {
-    string temp;
-    cout << "Enter strings, Ctrl+z to end input : " << endl;
-    while (getline(cin, temp))
+    std::string temp;
+    std::cout << "Enter strings, Ctrl+z to end input : " << std::endl;
+    while (std::getline(std::cin, temp))
     {
         reverse(temp.begin(), temp.end());
         myString.push_back(temp);
@@ -22,20 +20,14 @@ void reverseAndSort(vector<string>& myString)
 
 }
 
-const void printVector(const vector<string>& myString)
+const void printVector(const std::vector < std::string > & myString)
 {
-    for (auto i : myString)
-        cout << i << ' ' << endl;
-}
-//remove all elements from the vector and then reduce container capacity to fit its size.
-void vectorObliterate(vector<string>& myVector)
-{
-    myVector.clear();  myVector.shrink_to_fit();
+    for (auto& i : myString)
+        std::cout << i << ' ' << std::endl;
 }
 int main()
 {
-    vector<string> myString;
+    std::vector<std::string> myString;
     reverseAndSort(myString);
     printVector(myString);
-    vectorObliterate(myString);
 }
