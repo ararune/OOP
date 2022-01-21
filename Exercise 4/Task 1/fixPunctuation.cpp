36c2c91691ea95f66ad1ad2153b0e9e33b0c0777
Josip Condic Jurkic <51995137+ararune@users.noreply.github.com>
2022-01-21 13:01:58 +0100

    Add files via upload

---
+++ Exercise 4/Task 1/fixPunctuation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+
+typedef string cont;
+typedef cont::const_iterator iter;
+
+void fixPunctuation(cont& sentence) {
+    for (iter i = sentence.begin(); i != sentence.end(); ++i) {
+        if (*i == ' ' && (*(i + 1) == ',') || *(i + 1) == '.') {
+            sentence.erase(i);
+        }
+        if (*i == ',' && *(i + 1) != ' ') {
+            sentence.insert(i + 1, ' ');
+        }
+    }
+}
+
+int main() {
+    cont sentence = "I would ,if at all possible , like to print this sentence correctly .";
+    fixPunctuation(sentence);
+    cout << sentence << endl;
+
+}
